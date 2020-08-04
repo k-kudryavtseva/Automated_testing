fcb490dee53e83ef114e8ff331a4dada94369d5b
Kristina Kudryavtseva <kr.kudryavtseva@gmail.com>
2020-08-04 23:07:43 +0300

    created tests for sign up

---
+++ carina_ui/src/main/java/com/solvd/carina/demo/locators/SignUpPageLocators.java
@@ -0,0 +1,4 @@
+package com.solvd.carina.demo.locators;
+
+public class SignUpPageLocators {
+}
---
+++ carina_ui/src/main/java/com/solvd/carina/demo/locators/SignUpWindowLocators.java
@@ -0,0 +1,17 @@
+package com.solvd.carina.demo.locators;
+
+import org.openqa.selenium.By;
+
+public class RegisterPageLocators {
+    public static class SignUpForm {
+        public static final By SIGNUP_FORM_PARENT = By.id("user-submit");
+        public static final By CREATE_ACCOUNT_TITLE = By.xpath("//p[text()='Create account']");
+        public static final By NICKNAME_INPUT = By.xpath("//input[@id='uname']");
+        public static final By NICKNAME_MESSAGE = By.id("nick-reason");
+        public static final By EMAIL_INPUT = By.xpath("//div[@id='user-submit']//input[@id='email']");
+        public static final By PASSWORD_INPUT = By.xpath("//div[@id='user-submit']//input[@id='upass']");
+        public static final By AGREE_DATASTORAGE_BUTTON = By.xpath("//input[@id='iagree1' and @class='round-checkbox']");
+        public static final By AGREE_AGE_LIMIT_BUTTON = By.xpath("//input[@id='iagree2' and @class='round-checkbox']");
+        public static final By SUBMIT_BUTTON = By.xpath("//div[@id='ucsubmit-f']/input[@type='submit' and @id='nick-submit']");;
+    }
+}
